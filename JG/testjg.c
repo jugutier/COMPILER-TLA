7a36d7bce7c71cdbb36fcba3189b6711d60ec282
Jugutier <julian.3.1@gmail.com>
2013-06-26 14:28:00 -0300

    mejoras en compile

--- JG/testjg.c
+++ JG/testjg.c
@@ -3,6 +3,6 @@ int main ( ) {
 	decompose(1234);
 	decompose(3213132131);
 	decompose(654984135);
-	decompose(3435245496510598484357);
-	decompose(9876545649876537256574859);
+	decompose(343524549651059);
+	decompose(98765456498765372);
 }
